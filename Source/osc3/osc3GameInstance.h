36ac3566cb31238dbf7841a9ab153cdd1fd458bb
will floyd <floyd.will@gmail.com>
2024-04-27 21:48:06 -0700

    use custom GameInstance class

---
+++ Source/osc3/osc3GameInstance.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "osc3GameInstance.generated.h"
+
+UCLASS()
+class OSC3_API Uosc3GameInstance : public UGameInstance {
+	GENERATED_BODY()
+	
+};
