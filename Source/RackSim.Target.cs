3c91ee02f94430cb173bc029f09575b2254c7c4f
will floyd <floyd.will@gmail.com>
2024-07-13 09:11:38 -0700

    enable logging in shipping builds

--- Source/RackSim.Target.cs
+++ Source/RackSim.Target.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 
 public class RackSimTarget : TargetRules
 {
-	public RackSimTarget( TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.Add("RackSim");
-	}
+  public RackSimTarget(TargetInfo Target) : base(Target)
+  {
+    Type = TargetType.Game;
+    DefaultBuildSettings = BuildSettingsVersion.V2;
+    IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+    ExtraModuleNames.Add("RackSim");
+
+    bUseLoggingInShipping = true;
+  }
 }
