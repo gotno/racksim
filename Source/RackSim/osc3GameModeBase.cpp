6674f513a49e0b39cd9344a1f2cc34bbc9d1a26c
will floyd <floyd.will@gmail.com>
2024-07-20 15:52:44 -0700

    give initial module position an ergonomic tilt

--- Source/RackSim/osc3GameModeBase.cpp
+++ Source/RackSim/osc3GameModeBase.cpp
@@ -615,7 +615,7 @@ void Aosc3GameModeBase::SpawnModule(VCVModule vcv_module) {
     location = FVector(100.f, vcv_module.box.pos.x, -vcv_module.box.pos.y + 100.f);
     location.Y += vcv_module.box.size.x / 2;
     location.Z += vcv_module.box.size.y / 2;
-    rotation = FRotator(0.f);
+    rotation = FRotator(-40.f, 0.f, 0.f);
   }
 
   AVCVModule* module =
